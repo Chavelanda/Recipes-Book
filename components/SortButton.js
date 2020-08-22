9bad6eebd2737e33728a24c039599c1961907afb
Giacomo <gmelacini@unibz.it>
2020-08-22 18:25:16 +0200

    PropTypes added

--- components/SortButton.js
+++ components/SortButton.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
-
+import PropTypes from 'prop-types'
 
 const SortButton = (props) => {
+
   return (
     <View style={styles.container}>
       <Icon name={`ios-arrow-drop${props.up ? 'up' : 'down'}`} type='ionicon' color={props.color || 'black'} />
@@ -18,5 +19,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   }
 })
+
+SortButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  up: PropTypes.bool.isRequired,
+  color: PropTypes.string,
+}
 
 export default SortButton
--- components/SortButtonGroup.js
+++ components/SortButtonGroup.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Text, StyleSheet } from 'react-native';
 import { ButtonGroup } from 'react-native-elements';
+import PropTypes from 'prop-types'
 
 import SortButton from './SortButton'
 
@@ -10,7 +11,6 @@ export default class SortButtonGroup extends React.Component {
     selectedIndex: 0
   }
 
-
   updateIndex = (selectedIndex) => {
     const index = this.state.selectedIndex
 
@@ -18,15 +18,16 @@ export default class SortButtonGroup extends React.Component {
       this.props.buttons[selectedIndex].up = !this.props.buttons[selectedIndex].up
     }
 
+    this.props.onSortButtonPress(selectedIndex)
+
     this.setState({ prevSelectedIndex: index, selectedIndex: selectedIndex });
   }
 
   mapButton = (button, index) => ({
     element: () => index===this.state.selectedIndex ?
       <SortButton name={button.name} up={button.up} color='white' /> :
-      <SortButton name={button.name} up={button.up} color={this.props.color} />
+      <SortButton name={button.name} up={button.up} color={this.props.color || 'black'} />
   })
-
 
   render() {
     return (
@@ -39,4 +40,10 @@ export default class SortButtonGroup extends React.Component {
       />
     );
   }
+}
+
+SortButtonGroup.propTypes = {
+  buttons: PropTypes.array.isRequired,
+  color: PropTypes.string,
+  onSortButtonPress: PropTypes.func.isRequired
 }
